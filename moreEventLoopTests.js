38625a86acfbcaa3ae1c32f52a5331e5cda06b0d
Nikhil Narra <nikhilnarra@vt.edu>
2024-05-18 19:43:18 -0400

    adding more exercise files

---
+++ moreEventLoopTests.js
@@ -0,0 +1,22 @@
+console.log('1');
+
+setTimeout(function() {
+    console.log('2');
+    Promise.resolve().then(function() {
+        console.log('3');
+    });
+}, 500);
+
+setTimeout(function() {
+    console.log('4');
+}, 200);
+
+new Promise(function(resolve, reject) {
+    console.log('5');
+    resolve();
+}).then(function() {
+    console.log('6');
+});
+
+console.log('7');
+
